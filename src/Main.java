28a568b0dfa124dc291a519ee463cb5721f93b89
Ace <haroldace12345@gmail.com>
2024-11-26 23:00:58 -0800

    zxdx

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrameExt f = new JFrameExt();
+
+        f.setSize(400, 200);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.setVisible(true);
     }
 }
